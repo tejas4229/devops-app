828958e57a3e64aeeefc02c3f01514057c26d2e0
tejas4229 <77134626+tejas4229@users.noreply.github.com>
2026-01-20 00:09:55 +0530

    Update index.js
    saa

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const http = require('http');
 http.createServer((req, res) => {
-  res.end("your Project Running Successfully");
+  res.end("lay xala Project Running Successfully");
 }).listen(3000);
